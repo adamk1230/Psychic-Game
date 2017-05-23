f2afdca57d6d9a94909c4ff9d98f779391951116
Adam Kwan <adamk1230@gmail.com>
2017-05-23 19:43:43 -0400

    made var computerGUess global var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 
 // Define Variable
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Generates computer guess 
+var computerGuess = options[Math.floor(Math.random() * options.length)];
+console.log("Computer Guess: " + computerGuess);
+
+
 var wins = 0;
 var losses = 0;
 var guesses = 10;
+
 
 
 // Function to be triggered on key up
@@ -14,24 +21,27 @@ document.onkeyup = function(event) {
 	var userGuess = event.key;
 	alert(userGuess);
 
-	//Generates computer guess 
-	var computerGuess = options[Math.floor(Math.random() * options.length)];
-	alert(computerGuess);
+
 
 	console.log(guesses);
 
 	//Conditions
-	if (userGuess === computerGuess) {wins++;} 
+	if (userGuess === computerGuess) {
+		wins++;
+		var computerGuess = options[Math.floor(Math.random() * options.length)];
+	} 
 	else {guesses--;}
 
-	// if (guesses = 0) {
-	// 	losses++;
-	// 	guesses = 10;
-	// }
+	if (guesses === 0) {
+		losses++;
+		guesses = 10;	
+	}
 
 		console.log("wins: " + wins);
 		console.log("losses: " + losses);
 		console.log("guesses: " + guesses);
+		console.log("Computer Guess: " + computerGuess);
+		
 	
 
 
