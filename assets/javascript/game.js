30d6af6eea71eebfe86f104ddd648a57c012f2b8
Adam Kwan <adamk1230@gmail.com>
2017-05-23 20:36:03 -0400

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 
-// Define Variable
+// Define Variables
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Generates computer guess 
 var computerGuess = options[Math.floor(Math.random() * options.length)];
-console.log("Computer Guess: " + computerGuess);
-
 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
+//Lists guesses
+
+var guessArray = [];
 
 
 
@@ -19,17 +20,16 @@ document.onkeyup = function(event) {
 
 	//Determins which key was pressed
 	var userGuess = event.key;
-	alert(userGuess);
-
-
-
-	console.log(guesses);
+	guessArray.push(userGuess);
 
 	//Conditions
 	if (userGuess === computerGuess) {
 		wins++;
-		var computerGuess = options[Math.floor(Math.random() * options.length)];
+		guesses = 10;
+		computerGuess = options[Math.floor(Math.random() * options.length)];
+		guessArray = [];
 	} 
+
 	else {guesses--;}
 
 	if (guesses === 0) {
@@ -37,16 +37,21 @@ document.onkeyup = function(event) {
 		guesses = 10;	
 	}
 
+		// Allows me to monitor game in console
 		console.log("wins: " + wins);
 		console.log("losses: " + losses);
 		console.log("guesses: " + guesses);
 		console.log("Computer Guess: " + computerGuess);
-		
-	
 
 
+	// Displays game results on HTML
+	var updateWins = "<p>Wins: " + wins + "</p>";
+    var updateLosses = "<p>Losses: " + losses + "</p>";
+    var updateGuesses = "<p>Guesses left: " + guesses + "</p>";
+    var yourGuesses = "<p>Your guesses so far: " + guessArray + "</p>";
 
-
+    var gameDiv = document.getElementById('game');
+    gameDiv.innerHTML = updateWins + updateLosses + updateGuesses + yourGuesses;
 
 
 } //ends function
